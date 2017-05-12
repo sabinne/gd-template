cc7a11926f25ce4ad91dff507d6f3685703fbb7b
sabinne <sabin7sabin7sabin@gmail.com>
2017-05-12 11:55:05 -0400

    Create decisions.py

---
+++ code/decisions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+vars = 1
+
+if (vars == 1 ) : print "good" #
+
+if (vars == 1 ) : print "great"
+if (vars == 1 ) : print "amazing" #
+
+if (vars == 2  ) : print "amazing"
+else: print "bad"
