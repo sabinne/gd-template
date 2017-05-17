67166b4df63e71973e0b434968fc39a857e922cb
sabinne <sabin7sabin7sabin@gmail.com>
2017-05-17 09:16:30 -0500

    Add files via upload

---
+++ code/elif.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+vars = 1
+if vars == 2:   #
+ print "yes"
+ print vars
+elif vars == 3:   #
+ print "yes"
+ print vars
+elif vars == 1:   #
+ print "yes"
+ print vars
+elif vars == 1:   #
+ print "yes"
+else:                   # 
+ print "no vars here"
