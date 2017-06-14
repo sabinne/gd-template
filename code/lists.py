e23810b0f14c1d3c59de5925f77f0bac66ebebda
sabinne <sabin7sabin7sabin@gmail.com>
2017-06-14 14:47:09 -0400

    Create lists.py

---
+++ code/lists.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+race = ['winner', 'secondPlacer', 'thirdPlacer', 'fourthPlacer', 'loser'];  #  This is an example of a list with correct syntax, and so is the second line.
+racers = ['John', 'Jack', 'Sabin', 'Riley', 'James'];
+#  |  Down here is an example of how lists can be used to conduct the results of a race, for example these may be useful in a racing game
+#  V
+print racers[1] + " is the" + race[0] + racers[3] + " is the" + race[1] + racers[1] + " is the" + race[2] + racers[4] + " is the" + race[3] + " And" + racers[2] + " placed as the" + race[4]
+
+# Now lets see if the results are satisfactory  :D
